e5e24d6b66a8ffc1f70b9b427f9d9773ef7f1abf
nguyen phuc chuong <nguyenphucchuong97@gmail.com>
2020-04-15 15:51:43 +0700

    run within docker container

--- src/common/constant.js
+++ src/common/constant.js
@@ -1,4 +1,4 @@
-const SERVER = 'http://parking-sharing-be.ga:8000/';
+const SERVER = 'http://parking-sharing-be.ga:30000/';
 const STATUS = { PENDING: 0, CANCELED: 1, APPROVED: 2, REJECT: 3 };
 const ROLE = { 1: 'User', 2: 'Admin', 3: 'Super Admin' };
 export { SERVER, STATUS, ROLE };
